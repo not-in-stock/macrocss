8bd0de6d8a8b4a0ff5cef0bc43b21b133d95fc7c
Andrey Fedorov <oran9e.red@gmail.com>
2022-06-27 03:46:37 +0200

    Remove redundant test

--- test/stylo/core.clj
+++ test/stylo/core.clj
@@ -3,30 +3,6 @@
             [stylo.core :as stylo]))
 
 (deftest class-generation
-  (with-redefs [stylo/styles (atom {})
-                stylo/media-styles (atom {})]
-    (are [created-class] (true? created-class)
-      (c? [:text :blue-300] [:smartphone [:text :blue-500]])
-      (c? [:smartphone [:text :blue-500] {:font-weight "500"}]
-          [:screen [:text :pink-200] {:font-weight "300"}])
-      (c? [:smartphone [:bg :red-500] [[:.some-arbitrary-class {:bg :blue-400}]]])
-      (c? [:progress-bar [:bg :red-500]] {:font-weight "500"})
-      (c? [:progress-bar [:bg :red-500]])
-      (c? [:disabled [:hover [:bg :red-500]]])
-      (c? [:bg :red-500] [[:.some-arbitrary-class {:bg :blue-400}]])
-      (c? [:bg :red-500]
-          [:hover [[:.some-arbitrary-class {:bg :blue-400}]]])
-      (c? [:pseudo ":nth-child(2)" [:hover [:bg :red-500]]])
-      (c? [[:& {:color "red"}]
-           [:&:target {:color "green"}]])
-      (c? {:color "red"})
-      (c? [:hover [:placeholder :white] [:mb 1]])
-      (c? [:p 1])
-      (c? [:placeholder :white])
-      (c? [:divide-x 2])
-      (c? :sr-only))))
-
-(deftest class-generation
   (let [styles (atom {})
         media-styles (atom {})]
     (with-redefs [stylo/styles styles
